eb4c185f87b228268441cce4268a79bfe8ecc784
Guilherme Ribeiro (GR) <its.gr6603@gmail.com>
2023-09-28 14:40:10 -0300

    🚀

--- database.sql
+++ database.sql
@@ -1,2 +1,37 @@
-CREATE DATABASE gr;
-CREATE TABLE `gr`.`funcionarios` (`id` INT NOT NULL AUTO_INCREMENT , `nome` VARCHAR(255) NOT NULL , `cpf` VARCHAR(11) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema gr
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema gr
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `gr` DEFAULT CHARACTER SET utf8 ;
+-- -----------------------------------------------------
+-- Schema meunome
+-- -----------------------------------------------------
+USE `gr` ;
+
+-- -----------------------------------------------------
+-- Table `gr`.`colaboradores`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `gr`.`colaboradores` (
+  `id` INT NOT NULL,
+  `nome` VARCHAR(255) NULL,
+  `telefone` INT NULL,
+  `email` VARCHAR(255) NULL,
+  `cargo` VARCHAR(255) NULL,
+  `salario` FLOAT NULL,
+  `cidade` VARCHAR(255) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
--- index.php
+++ index.php
@@ -21,11 +21,30 @@ if (!$conexao) {
 $id = $_POST["id"];
 $nome = $_POST["nome"];
 $cpf = $_POST["cpf"];
+$telefone = $_POST["telefone"];
+$email = $_POST["email"];
+$cargo = $_POST["cargo"];
+$salario = $_POST["salario"];
+$cidade = $_POST["cidade"];
+$contratado = $_POST["contratado"];
 $botao = $_POST["botao"];
 
-// echo $id." ".$nome." ".$cpf." ".$botao
+if (empty($botao)) {
+} else if ($botao == "Cadastrar") {
+    $sql = "INSERT INTO colaboradores(id, nome, cpf) VALUES ('', '$nome', '$cpf')";
+}
 
-$sql = "INSERT INTO funcionarios (id, nome, cpf) VALUES ('')"
+// tratando erros das operações C.E.R.A
+if (!empty($sql)){
+    if(mysqli_query($conexao, $sql)){
+        echo "Operação realizada com sucesso";
+    } else {
+        echo "Houve um erro na operação: <br />";
+        echo mysqli_error($conexao);
+    }
+}
+
+// echo $id." ".$nome." ".$cpf." ".$botao
 
 ?>
 
@@ -44,7 +63,7 @@ $sql = "INSERT INTO funcionarios (id, nome, cpf) VALUES ('')"
         <label>ID<input type="text" name="id" /></label>
         <label>Nome<input type="text" name="nome" /></label>
         <label>CPF<input type="text" name="cpf" /></label>
-        
+
         <input type="submit" name="botao" value="Cadastrar" />
         <input type="reset" name="botao" value="Cancelar" />
 
